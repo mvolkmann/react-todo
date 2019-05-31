72e238dc3c30a533cf0f97993e986ba56243f1fd
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-05-31 15:06:05 -0500

    initial commit

---
+++ src/todo-list.js
@@ -0,0 +1,84 @@
+import React, {Component} from 'react';
+import Todo from './todo';
+import './todo.css';
+
+let lastId = 0;
+
+const createTodo = (text, done = false) => ({id: ++lastId, text, done});
+
+class TodoList extends Component {
+  state = {
+    todoText: '',
+    todos: [createTodo('learn React', true), createTodo('build a React app')]
+  };
+
+  get uncompletedCount() {
+    return this.state.todos.filter(t => !t.done).length;
+  }
+
+  onAddTodo = () =>
+    this.setState(state => ({
+      todoText: '',
+      todos: state.todos.concat(createTodo(state.todoText))
+    }));
+
+  onArchiveCompleted = () =>
+    this.setState(state => ({
+      todos: this.state.todos.filter(t => !t.done)
+    }));
+
+  onDeleteTodo = todoId =>
+    this.setState(state => ({
+      todos: this.state.todos.filter(t => t.id !== todoId)
+    }));
+
+  onTextChange = event => this.setState({todoText: event.target.value});
+
+  onToggleDone = todo =>
+    this.setState(state => {
+      const {id} = todo;
+      const todos = state.todos.map(t =>
+        t.id === id ? {...t, done: !t.done} : t
+      );
+      return {todos};
+    });
+
+  render() {
+    const {todos, todoText} = this.state;
+    const todoElements = todos.map(todo => (
+      <Todo
+        key={todo.id}
+        todo={todo}
+        onDeleteTodo={() => this.onDeleteTodo(todo.id)}
+        onToggleDone={() => this.onToggleDone(todo)}
+      />
+    ));
+
+    return (
+      <div>
+        <h2>To Do List</h2>
+        <div>
+          {this.uncompletedCount} of {todos.length} remaining
+          <button onClick={this.onArchiveCompleted}>Archive Completed</button>
+        </div>
+        <br />
+        <form>
+          <input
+            type="text"
+            size="30"
+            autoFocus
+            placeholder="enter new todo here"
+            value={todoText}
+            onChange={this.onTextChange}
+          />
+          <button disabled={!todoText} onClick={this.onAddTodo}>
+            Add
+          </button>
+        </form>
+        <ul className="unstyled">{todoElements}</ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
---
+++ src/todo.js
@@ -0,0 +1,22 @@
+import {bool, func, shape, string} from 'prop-types';
+import React from 'react';
+
+// A props object is passed to this function and destructured.
+const Todo = ({onDeleteTodo, onToggleDone, todo}) => (
+  <li>
+    <input type="checkbox" checked={todo.done} onChange={onToggleDone} />
+    <span className={'done-' + todo.done}>{todo.text}</span>
+    <button onClick={onDeleteTodo}>Delete</button>
+  </li>
+);
+
+Todo.propTypes = {
+  onDeleteTodo: func.isRequired,
+  onToggleDone: func.isRequired,
+  todo: shape({
+    done: bool.isRequired,
+    text: string.isRequired
+  }).isRequired
+};
+
+export default Todo;
